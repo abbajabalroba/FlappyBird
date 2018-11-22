fe099fb049c5032f5c99808a91292bed49bdd34d
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2018-11-21 18:22:26 -0600

    Initial commit.

---
+++ Assets/BirdyControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdyControl : MonoBehaviour
+{
+    public Transform tf;
+    public Rigidbody2D rb;
+    public bool IsJumping;
+    public float time = 3.0f;
+
+    // Use this for initialization
+    public void Start ()
+    {
+        IsJumping = false;
+    }
+	
+	// Update is called once per frame
+	public void FixedUpdate ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * 400);
+        }
+	}
+
+    IEnumerator Wait(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+}
---
+++ Assets/PipeMoving.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoving : MonoBehaviour
+{
+    public GameObject pipes;
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb.AddForce(-Vector2.right * 4000);
+    }
+}
